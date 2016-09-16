e52bb40c62673ce875708e76cd4d1b1a2cf3aaca
Damien Dunn <damienhoward95@gmail.com>
2016-09-16 11:41:33 -0400

    Made a few changes

--- firebase/app/App/Firebase.py
+++ firebase/app/App/Firebase.py
@@ -1,11 +1,7 @@
-import http
-import urllib
-from http import client
-from urllib import request
+import http,urllib.request
 
-
-class firebase():
-    def __init__(self, database_url, api_key=None, auth_domain=None, storage_bucket=None):
+class firebase(object):
+    def __init__(self, database_url ,api_key=None, auth_domain=None, storage_bucket=None):
         # TODO change the following configuration system to config
         """
         :param api_key: unique api key of user
@@ -17,15 +13,19 @@ class firebase():
         self.auth_domain = auth_domain
         self.database_url = database_url
         self.storage_bucket = storage_bucket
-
+        self.config = {"apiKey": self.api_key,
+                        "authDomain": self.auth_domain,
+                        "databaseURL": self.database_url,
+                        "storageBucket": self.storage_bucket
+                      }
     def initialize_app(self):
-        config = {"apiKey": self.api_key,
+        self.config = {"apiKey": self.api_key,
                   "authDomain": self.auth_domain,
                   "databaseURL": self.database_url,
                   "storageBucket": self.storage_bucket
                   }
-        connection = request.urlopen(self.database_url)
-        print(connection)
+        connection = urllib.request.urlopen(self.database_url)
+        print(connection.read())
 
         # return the connection variable
         return firebase
@@ -34,5 +34,3 @@ class firebase():
 firebase = firebase("https://testing-6aa1e.firebaseio.com/",
                     api_key="python-firebase-e17d0.firebaseapp.com")
 firebase.initialize_app()
-
-
